b79768b24a2aa47825f174423d25378d76f6499c
Josh Gigliotti <43352647+jgigliotti38@users.noreply.github.com>
2024-02-13 16:58:35 -0500

    Update eldenring.sh
    adjustments but needs improvement

--- bashscripts/bashlearning/eldenring.sh
+++ bashscripts/bashlearning/eldenring.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
-clear
 
-echo "You Died"
 
-sleep 2
+# x_totalcards
+# y_cardsperpack
+# z_packsperplayer
+# n_numplayers
 
-# First Beast encounter
+# a_remainingcardpool
+# b_remainingpacks
+# c_remainingcards
 
-beast=$(( $RANDOM % 2 ))
 
-echo "Your first beast approaches. Prepare to battle. Pick a number between 0-1. (0/1)"
+echo "How many total cards in cube?"
+read x_totalcards 
+#echo "Total Cards in Cube: $totalcards"
 
-read tarnished
+echo "How many players?"
+read n_numplayers 
+#echo "Total Players: $players"
 
-if [[ $beast == $tarnished ]]; then
-	echo "Beast VANQUISHED!! Well done fellow tarnished"
+if [ -z "$1" ]; then
+    $y_cardsperpack = 15
 else
-	echo "You Died"
-	exit 1
+    $y_cardsperpack = $1
 fi
 
-sleep 2
-
-echo "."
-sleep 1
-echo ".."
-sleep 1
-echo "..."
-sleep 1
-
-echo "Boss Battle. Foul tarnished, Margit has arrived. Pick a number between 0-9. (0-9)"
-
-read tarnished
-
-margit=$(( RANDOM % 10 ))
-
-if [[ $beast == $tarnished || $tarnished == "cheat" ]]; then
-	echo "Great One  VANQUISHED!! Well done fellow tarnished"
+if [ -z "$2" ]; then
+    $z_packsperplayer = 3 
 else
-	echo "You Died"
+    $z_packsperplayer = $2
 fi
+
+
+echo "Calculating ..."
+sleep 3
+
+
+cardsperplayer=$(( $x_totalcards / $n_numplayers ))
+cardsremaining=$(($x_totalcards % $n_numplayers ))
+echo "Total Cards in Cube: $totalcards"
+echo "y = $y_cardsperpack"
+echo "z = $z_packsperplayer"
+echo "Each Player will draft: $cardsperplayer cards with $cardsremaining cards remaining"
