58da0548b3ffbd540401a2bb8d9f43fb701e4771
Josh Gigliotti <43352647+jgigliotti38@users.noreply.github.com>
2024-02-13 16:50:38 -0500

    Update cube.sh
    adding extra variables

--- bashscripts/cube.sh
+++ bashscripts/cube.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-echo "How many players?"
-read players
-echo "Total Players: $players"
+
+# x_totalcards
+# y_cardsperpack
+# z_packsperplayer
+# n_numplayers
+
+# a_remainingcardpool
+# b_remainingpacks
+# c_remainingcards
 
 
 echo "How many total cards in cube?"
-read totalcards
+read x_totalcards 
 #echo "Total Cards in Cube: $totalcards"
+
+echo "How many players?"
+read n_numplayers 
+#echo "Total Players: $players"
+
+if [-z $1]; then
+    $y_cardsperpack = 15
+else
+    $y_cardsperpack = $1
+fi
+
+y_cardsperpack = $1
+z_packsperplayer = $2
+
 
 echo "Calculating ..."
 sleep 3
@@ -16,4 +36,4 @@ sleep 3
 cardsperplayer=$(( $totalcards / $players ))
 cardsremaining=$(($totalcards % $players))
 echo "Total Cards in Cube: $totalcards"
-echo "Each Player will draft: $cardsperplayer cards with $cardsremaining cards remaining"
+echo "Each Player will draft: $cardsperplayer cards with $cardsremaining cards remaining
