c4b73805c5e38a53655d1f182688ae34264cc569
Joshua Gigliotti <jgigliotti@claytonkendall.com>
2024-02-15 12:51:29 -0500

    testing cube.py file

--- Python/cube/cube.py
+++ Python/cube/cube.py
@@ -6,7 +6,8 @@ a suitable cube based on various inputs
 and customization options.
 """
 
-
-
 print("========== CUBE TIME ==========")
 
+x_totalcards = input("Enter the Number of Cards in your cube\n")
+
+print(x_totalcards)
