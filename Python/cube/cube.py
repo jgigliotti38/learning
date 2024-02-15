16eb1c4ca2de4b65418cb3d8383c359115c22ce4
Joshua Gigliotti <jgigliotti@claytonkendall.com>
2024-02-15 11:51:41 -0500

    created a cube folder for cube.py

---
+++ Python/cube/cube.py
@@ -0,0 +1,12 @@
+# cube.py
+"""
+Welcome,
+The goal for this program is to create
+a suitable cube based on various inputs
+and customization options.
+"""
+
+
+
+print("========== CUBE TIME ==========")
+
