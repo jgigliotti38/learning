cd8f4f49579f6dab1595c9ade0b65c593ce200e3
Joshua Gigliotti <jgigliotti@claytonkendall.com>
2024-02-16 15:11:54 -0500

    added scrips to save

---
+++ Powershell/Untitled1.ps1
@@ -0,0 +1,16 @@
+﻿$computername = $env:COMPUTERNAME
+
+$UninstallKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+
+$reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computername)
+
+$regkey=$reg.OpenSubKey($UninstallKey)
+
+$subkeys=$regkey.GetSubKeyNames()
+
+foreach($key in $subkeys) {
+    $thiskey=$UninstallKey+"\\"+$key
+    $thisSubKey=$reg.OpenSubkey($thiskey)
+    $DisplayName=$thisSubKey.GetValue("DisplayName")
+    Write-Host $computername, $DisplayName
+}
---
+++ Powershell/Untitled3.ps1
@@ -0,0 +1,8 @@
+﻿$app = Get-WmiObject -Class Win32_Product | Where-Object {
+    $_.Name -match "Zoom"
+}
+
+Write-Host Complete
+Pause
+
+$app.Uninstall()
